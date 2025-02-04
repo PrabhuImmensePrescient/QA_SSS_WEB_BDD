6838ef4d74c553881306408c933a96f5dd6f3ccf
Amar <amar.848301@gmail.com>
2025-02-04 22:41:14 +0530

    first commit

---
+++ src/test/java/stepDefinitions/LoginStepDefinition.java
@@ -0,0 +1,26 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinition {
+	
+	@Given("the user is on the login page")
+	public void the_user_is_on_the_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("login into portal with {string} credential")
+	public void login_into_portal_with_credential(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("the user should be redirected to the {string} page")
+	public void the_user_should_be_redirected_to_the_page(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
